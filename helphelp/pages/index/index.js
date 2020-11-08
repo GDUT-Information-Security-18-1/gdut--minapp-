f6be66b17921f0b5b9027e25fd4c0710dbe8b087
0Oo-oO0 <46864199+0Oo-oO0@users.noreply.github.com>
2020-11-09 01:14:55 +0800

    首页构造完成

--- helphelp/pages/index/index.js
+++ helphelp/pages/index/index.js
@@ -1,38 +1,45 @@
 //Page Object
 Page({
   data: {
-    
+    catesList: [{
+        id: 0,
+        name: "帮我买",
+        image_src: "../../icons/buy.png",
+        text_src: "../../icons/buy_text.png"
+      },
+      {
+        id: 1,
+        name: "帮我送",
+        image_src: "../../icons/send.png",
+        text_src: "../../icons/send_text.png"
+      }
+    ],
+    tabs: [{
+      id: 0,
+      value: "综合",
+      isActive: true
+    }, {
+      id: 1,
+      value: "时间",
+      isActive: false
+    }, {
+      id: 2,
+      value: "价格",
+      isActive: false
+    }]
   },
-  //options(Object)
-  onLoad: function(options){
-    
-  },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
 
-  },
-  onUnload: function(){
-
-  },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
+  handleTabsItemChange(e) {
+    const {
+      index
+    } = e.detail;
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({
+      tabs
+    })
   }
+
 });
